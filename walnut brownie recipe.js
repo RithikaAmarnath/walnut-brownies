3395d64c6004f98a6687c45a6f941523744b6893
RithikaAmarnath <76592182+RithikaAmarnath@users.noreply.github.com>
2021-03-25 19:15:48 +0530

    Add files via upload

---
+++ walnut brownie recipe.js
@@ -0,0 +1,25 @@
+all-purpose flour (about 3/4 cup)
+
+1 cup granulated sugar
+
+¾ cup unsweetened cocoa
+
+½ cup packed brown sugar
+
+½ teaspoon baking powder
+
+¼ teaspoon salt
+
+1 cup bittersweet chocolate chunks, divided
+
+⅓ cup fat-free milk
+
+6 tablespoons butter, melted
+
+1 teaspoon vanilla extract
+
+2 large eggs, lightly beaten
+
+½ cup chopped walnuts, divided
+
+Cooking spray
